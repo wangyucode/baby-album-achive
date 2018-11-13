1568ca33e9c238531f89f2934ff1155c4d038cf9
wycode <www_wayne_001@163.com>
2018-11-13 11:32:43 +0800

    删除相册

--- pages/album/album-detail/album-detail.js
+++ pages/album/album-detail/album-detail.js
@@ -20,7 +20,9 @@ Page({
     members: [],
     canLoadMore: false,
     currentPage: 0,
-    pageSize: 14
+    pageSize: 15,
+    isOwner:false,
+    isFromNewPhoto:false
   },
 
   /**
@@ -35,7 +37,6 @@ Page({
       ownerIcon: options.ownerIcon
     })
     this.data.nameChange = this.data.name;
-    
     wx.startPullDownRefresh();
   },
 
@@ -50,10 +51,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if (this.data.readPermission) {
-      this.getAlbumPhotos();
+    if(this.data.isFromNewPhoto){
+      this.data.isFromNewPhoto = false
+      wx.startPullDownRefresh();
     }
-
   },
 
   /**
@@ -95,18 +96,19 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    let image = this.data.cover;
-    if (image.length == 0) {
-      image = "/assets/no_photo.png"
-    } else {
-      image = "https://wycode-baby-album.oss-cn-zhangjiakou.aliyuncs.com/" + image
+    if (this.data.sharePermission) {
+      let image = this.data.cover;
+      if (image.length == 0) {
+        image = "/assets/no_photo.png"
+      } else {
+        image = "https://wycode-baby-album.oss-cn-zhangjiakou.aliyuncs.com/" + image
+      }
+      return {
+        title: app.globalData.userInfo.nickName + ' 邀请你成为《' + this.data.name + '》的成员！',
+        path: "/pages/album/join-album/join-album?id=" + this.data.id + "&time=" + new Date().getTime(),
+        imageUrl: image
+      }
     }
-    return {
-      title: app.globalData.userInfo.nickName + ' 邀请你成为《' + this.data.name + '》的成员！',
-      path: "/pages/album/join-album/join-album?id=" + this.data.id + "&time=" + new Date().getTime(),
-      imageUrl: image
-    }
-
   },
 
   inputAlbumName: function (e) {
@@ -121,6 +123,7 @@ Page({
   },
 
   toUpload: function () {
+    this.data.isFromNewPhoto = true
     wx.navigateTo({
       url: './new-photo/new-photo?id=' + this.data.id
     })
@@ -221,9 +224,9 @@ Page({
             readPermission: ((res.data.data & 1) == 1),
             writePermission: ((res.data.data & 2) == 2),
             deletePermission: ((res.data.data & 4) == 4),
-            sharePermission: ((res.data.data & 8) == 8)
+            sharePermission: ((res.data.data & 8) == 8),
+            isOwner: ((res.data.data & 16) == 16)
           })
-          this.data.pageSize = this.data.writePermission ? 14 : 15
           this.data.currentPage = 0
           this.data.photos = []
           if (this.data.readPermission) {
@@ -249,6 +252,79 @@ Page({
           })
         }
       }
+    })
+  },
+
+  onTapDelete: function () {
+    var that =this
+    wx.showModal({
+      title: '请确认！',
+      content: '确定删除此相册吗？',
+      success(res) {
+        if (res.confirm) {
+          that.deleteAlbum();
+        }
+      }
+    })
+  },
+
+  deleteAlbum:function(){
+    wx.showLoading({
+      title: '请稍候...',
+      mask: true
+    })
+
+    wx.request({
+      url: 'https://wycode.cn/web/api/public/album/deleteAlbum',
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        'accessKey': app.globalData.accessKey,
+        'albumId': this.data.id
+      },
+      success: function (res) {
+        console.log("deleteAlbum->", res)
+        if (res.statusCode == 200 && res.data.success) {
+          wx.showToast({
+            title: "删除成功!"
+          });
+          setTimeout(() => {
+            wx.navigateBack({})
+          }, 1200);
+        } else {
+          wx.showToast({
+            icon: "none",
+            title: res.data.error
+          })
+        }
+      }
+    })
+  },
+
+  toMemberDetail:function(e){
+    if(!this.data.isOwner){
+      wx.showToast({
+        icon: "none",
+        title: "只有相册主人可以修改权限！"
+      })
+      return
+    }
+    if(e.currentTarget.dataset.owner){
+      wx.showToast({
+        icon: "none",
+        title: "不能编辑相册主人权限！"
+      })
+      return
+    }
+    let member = this.data.members[e.currentTarget.dataset.index]
+    wx.navigateTo({
+      url: './member-detail/member-detail?albumId=' + this.data.id +
+        '&permission=' + member.permission +
+        '&id=' + member.id +
+        '&nickName=' + member.user.nickName +
+        '&avatarUrl=' + member.user.avatarUrl
     })
   }
 
